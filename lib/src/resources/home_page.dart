4b8419e2e7902146d5bad22d8be5c3d5afdee8ab
Xuan Huy <xuanhuy@gmail.com>
2020-07-22 09:25:29 +0700

    Google maps add in the Home Page

--- lib/src/resources/home_page.dart
+++ lib/src/resources/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,8 +12,44 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          child: Center(
-            child: Text("Hello"),
+          constraints: BoxConstraints.expand(),
+          color: Colors.white,
+          child: Stack(
+            children: <Widget>[
+              GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(10.7915178, 106.7271422),
+                  zoom: 14.4746,
+                ),
+              ),
+              Positioned(
+                left: 0,
+                top: 0,
+                right: 0,
+                child: Column(
+                  children: <Widget>[
+                    AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0.0,
+                      title: Text(
+                        "Taxi App",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      leading: FlatButton(
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        child: Icon(Icons.menu),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
